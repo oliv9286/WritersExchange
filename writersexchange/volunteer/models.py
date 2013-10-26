778e4d3b0efde569d9f2b19420bdd149946d79bc
Brandon Sanderson <singingboyo@hotmail.com>
2013-10-25 23:23:02 -0700

    Fix model, should generate things properly now

--- writersexchange/volunteer/models.py
+++ writersexchange/volunteer/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Volunteers(models.Model):
 	name = models.CharField(max_length=200)
-	email = models.CharField(max_length=200)
+	email = models.CharField(max_length=200, unique = True, primary_key=True)
 	phone = models.IntegerField(default=0)
 	address = models.CharField(max_length=500)
 	city = models.CharField(max_length=50)
@@ -11,9 +11,9 @@ class Volunteers(models.Model):
 	isApproved = models.BooleanField()
 	reference1name = models.CharField(max_length=200)
 	reference1email = models.CharField(max_length=200)
-	reference1phone = models.IntegerField(default=0)
+	reference1phone = models.CharField(max_length=10)
 	reference2name = models.CharField(max_length=200)
 	reference2email = models.CharField(max_length=200)
-	reference2phone = models.CharField(default=0)
+	reference2phone = models.CharField(max_length = 10)
 	experience = models.CharField(max_length=2500)
 	availability = models.CharField(max_length=2500)
