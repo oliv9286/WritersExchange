1566731016f04d784320b192bf8cdd43f85927b7
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2013-10-26 08:57:45 -0700

    moved db to a different directory

--- writersexchange/volunteer/views.py
+++ writersexchange/volunteer/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import redirect, render, get_object_or_404, render_to_response
+from django.core.urlresolvers import reverse
 from volunteer.models import Volunteer
 from volunteer.forms import ApplicationForm
 from django import forms
@@ -14,12 +15,20 @@ def apply(request):
 	if form.is_valid():
 		new_application = form.save()
 		new_application.save()
-		return HttpResponse("Success")
+		email = form.cleaned_data['email']
+		volunteer = get_object_or_404(Volunteer, email==email)
+
+		return HttpResponse("Thank you for applying, we will notify you through email when your application has been")
 	else: 
 	    return render_to_response('volunteer/apply.html',
                           {'application_form': form},
                           context_instance=RequestContext(request))
 
+# def confirmation(request, email):
+# 	match_volunteer = get_object_or_404(Volunteer, email==email)
+# 	return render_to_response('volunteer/confirm.html',
+# 							{'applicant':match_volunteer},
+# 							context_instance=RequestContext(request))
 
 
 
