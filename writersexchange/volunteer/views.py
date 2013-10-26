29e3d91ac83314457144b3e3bb556178b98b0824
Alison Clark <alison.marie.clark@gmail.com>
2013-10-26 11:12:29 -0700

    views.py: Fixed error in apply method.

--- writersexchange/volunteer/views.py
+++ writersexchange/volunteer/views.py
@@ -17,14 +17,14 @@ def apply(request):
 		new_application = form.save()
 		new_application.save()
 		email = form.cleaned_data['email']
-		volunteer = get_object_or_404(Volunteer, email==email)
+		volunteer = get_object_or_404(Volunteer, email=email)
                 # Send email to Stacy to inform her of new applicant
                 # TODO: replace '' with message body
                 # TODO: set EMAIL_BACKEND in settings file to the actual email backend
                 # TODO: change 'from@example.com' to actual address to send from
                 # TODO: change 'stacey@mailinator.com' to Stacey's email
                 send_mail('New Volunteer Application', '', 'from@example.com', ['stacey@mailinator.com'], fail_silently=False)
-		return HttpResponse("Thank you for applying, we will notify you through email when your application has been")
+		return HttpResponse("Thank you for applying, we will notify you through email when your application has been reviewed.")
 	else: 
 	    return render_to_response('volunteer/apply.html',
                           {'application_form': form},
