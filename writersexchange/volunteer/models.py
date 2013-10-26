73f570636977078b79546324b9d4ff0e4cb71286
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2013-10-25 23:12:38 -0700

    added model, but failed to create object in db

--- writersexchange/volunteer/models.py
+++ writersexchange/volunteer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Volunteers(models.Model):
+	name = models.CharField(max_length=200)
+	email = models.CharField(max_length=200)
+	phone = models.IntegerField(default=0)
+	address = models.CharField(max_length=500)
+	city = models.CharField(max_length=50)
+	province = models.CharField(max_length=20)
+	isApproved = models.BooleanField()
+	reference1name = models.CharField(max_length=200)
+	reference1email = models.CharField(max_length=200)
+	reference1phone = models.IntegerField(default=0)
+	reference2name = models.CharField(max_length=200)
+	reference2email = models.CharField(max_length=200)
+	reference2phone = models.CharField(default=0)
+	experience = models.CharField(max_length=2500)
+	availability = models.CharField(max_length=2500)
