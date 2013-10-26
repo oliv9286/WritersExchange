af326b220fa7bff8af3659995066d39dfe01dfdc
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2013-10-26 00:19:10 -0700

    changed some naming and synced db

--- writersexchange/volunteer/models.py
+++ writersexchange/volunteer/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class Volunteers(models.Model):
+class Volunteer(models.Model):
 	name = models.CharField(max_length=200)
-	email = models.CharField(max_length=200, unique = True, primary_key=True)
+	email = models.EmailField(max_length=100, unique = True, primary_key=True)
 	phone = models.IntegerField(default=0)
 	address = models.CharField(max_length=500)
 	city = models.CharField(max_length=50)
 	province = models.CharField(max_length=20)
-	isApproved = models.BooleanField()
+	isApproved = models.BooleanField(default=False)
 	reference1name = models.CharField(max_length=200)
-	reference1email = models.CharField(max_length=200)
+	reference1email = models.EmailField(max_length=200)
 	reference1phone = models.CharField(max_length=10)
 	reference2name = models.CharField(max_length=200)
-	reference2email = models.CharField(max_length=200)
+	reference2email = models.EmailField(max_length=200)
 	reference2phone = models.CharField(max_length = 10)
 	experience = models.CharField(max_length=2500)
 	availability = models.CharField(max_length=2500)
+
+	def is_approved(self):
+		return self.isApproved
+
+
+
--- writersexchange/writersexchange/settings.py
+++ writersexchange/writersexchange/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '../../writersexchange.db',                      # Or path to database file if using sqlite3.
+        'NAME': '../writersexchange.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -121,7 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'volunteer'
+    'volunteer',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
